c432648efa7b57ab8526b64acc65986353048087
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-03-01 17:06:17 +0300

    Rating_done

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, KeyboardEvent } from "react";
 import { RatingProps } from "./Rating.props";
 import styles from './Rating.module.css';
 import cn from "classnames";
-import StarIcon from './star.svg'
+import StarIcon from './star.svg';
 
 
 export const Rating = ({ isEditable = false, rating, setRating, className, ...props }: RatingProps): JSX.Element => {
@@ -12,21 +12,54 @@ export const Rating = ({ isEditable = false, rating, setRating, className, ...pr
     }, [rating]);
 
     const constructRating = (currentRating: number) => {
-        const updatedArray = ratingArray.map((rating: JSX.Element, index: number) => {
+        const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
             return (
-                < StarIcon className={cn(styles.star, {
-                    [styles.filled]: index < currentRating
+                <span className={cn(styles.star, {
+                    [styles.filled]: i < currentRating, // index равен 0 до 4, currentRating от 1 до 5, => условие строго меньше
+                    [styles.editable]: isEditable
                 })}
-                />
+                    onMouseEnter={() => changeDisplay(i + 1)}
+                    //Навели мышь - показали залитые звезды (индекс + 1)
+                    onMouseLeave={() => changeDisplay(rating)} //Убрали мышь - вернули в первоначальное отображение рейтинга
+                    onClick={() => onSetRating(i + 1)} // Устанавливаем значние рейтинга
+                >
+
+                    < StarIcon
+                        tabIndex={isEditable ? 0 : -1} // Когда будем табать по странице мы будем табать и по рейтингу если он Editable
+                        onKeyDown={(event: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, event)} //По нажатию на пробел устанавливается рейтинг (если он isEditable вызывается функция handleSpace)
+                    />
+                </span>
+
             );
         });
         setRatingArray(updatedArray);
-    }
-    // Функция чтобы заполнить стейт
+    };
+    // Функция чтобы заполнить стейт (r - rating/ i - index)
+    const changeDisplay = (i: number) => {
+        if (!isEditable) {
+            return;
+        }
+        constructRating(i);
+    };
+
+    const onSetRating = (i: number) => {
+        if (!isEditable || !setRating) {
+            return;
+        } // Если рейтинг не установлен и он у нас Editable
+        setRating(i);
+    };
+
+    const handleSpace = (i: number, event: KeyboardEvent) => {
+        if (event.code != 'Space' || !setRating) {
+            return;
+        } // Если нажатая кнопка не пробел то ничего не делать
+        setRating(i);
+    };
+
     return (
         <div {...props}>
-            {ratingArray.map((rating, index) => (<span key={index}>{rating}</span>))}
+            {ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
         </div>
     );
 };
-// [styles.fill]: index < currentRating - index варьируется от 0 до 4, currentRating от 1 до 5, поэтому условие строго меньше
+// [styles.fill]: index < currentRating - 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
 
 export default function Home(): JSX.Element {
 
-	const [counter, setCounter] = useState<number>(0);
+	const [rating, setRating] = useState<number>(4);
 
-	useEffect(() => {
-		console.log(`Запустилось основное body ${counter}`);
-		return function cleanup() {
-			console.log(`Запустился cleanup ${counter}`);
-		};
-		// eslint-disable-next-line
-	}, []);
 	return (
 		<>
-			<Htag tag='h1'>{counter}</Htag>
-			<Button appearance="primary" arrow="right" onClick={() => setCounter(counter + 1)}>Кнопка +1</Button>
-			<Button appearance="ghost" arrow="down" onClick={() => setCounter(counter - 1)}>Кнопка -1</Button>
+			<Htag tag='h1'>Заголовок</Htag>
+			<Button appearance="primary" arrow="right">Кнопка</Button>
+			<Button appearance="ghost" arrow="down">Кнопка</Button>
 			<P size="s">Маленький</P>
 			<P>Средний</P>
 			<P size="l">Большой</P>
@@ -24,7 +17,7 @@ export default function Home(): JSX.Element {
 			<Tag size='s' color="green" >Green</Tag>
 			<Tag size='s' color="primary" href="https://www.google.com" >Primary</Tag>
 			<Tag>Ничего</Tag>
-			<Rating rating={4} />
+			<Rating rating={rating} isEditable setRating={setRating} />
 		</>
 	);
 }
